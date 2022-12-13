bb74b4346df37f07223f00361d73c6c1de7dcd86
mannyzhuo <zgrbsbf@gmail.com>
2022-12-13 15:35:45 +0800

    auto hide address bar

--- auto_hide_address_bar_hover_show.js
+++ auto_hide_address_bar_hover_show.js
@@ -1,3 +1,57 @@
 /**
+ * add this file to browser.html directly or create  a js file and link it to browser.html with script tag
+ * the path of browser.html in my PC is "C:\Users\xxxx\AppData\Local\Vivaldi\Application\5.6.2867.40\resources\vivaldi\browser.html" 
+ */
 
-**/
+setTimeout(() => {
+    const header = document.querySelector('#header');
+    const browser = document.querySelector('#browser');
+    const mainbar=document.querySelector('.mainbar');
+
+    let time=null;
+    let out=111;
+
+    function show(){
+        browser.classList.remove('address-top-off')
+        browser.classList.add('address-top')
+    }
+
+    function hide(){
+        browser.classList.add('address-top-off')
+        browser.classList.remove('address-top')
+    }
+
+    header.addEventListener('mouseenter',function(){
+        console.log('header enter');
+        if(time){
+            clearTimeout(time);
+            time=null;
+        }
+
+        const i = document.querySelector('#browser').classList.contains('address-top')
+        if(i){
+
+        }else{
+            show()
+        }
+     })
+     header.addEventListener('mouseleave',function(){
+        console.log('leave0');
+       time= setTimeout(function(){
+           hide();
+        },out);
+     })
+     mainbar.addEventListener('mouseenter',function(){
+        if(time){
+            clearTimeout(time);
+            time=null;
+        }
+     })
+     mainbar.addEventListener('mouseleave',function(){
+        console.log('mainbar leave');
+        time= setTimeout(function(){
+           hide();
+        });
+     })     
+}, 1111);
+
