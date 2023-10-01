3ae7ac3864e9e30a31cadaea56e6e44e17e6ace3
mannyzhuo <zgrbsbf@gmail.com>
2023-10-01 20:49:29 +0800

    Update focus_input when_window_panel_open.js

--- focus_input when_window_panel_open.js
+++ focus_input when_window_panel_open.js
@@ -1,30 +1,27 @@
 setTimeout(() => {
   // Select the node that will be observed for mutations
-  const targetNode = document.querySelector("#main");
+  const targetNode = document;
 
   // Options for the observer (which mutations to observe)
   const config = { attributes: true, childList: true, subtree: true };
 
   // Callback function to execute when mutations are observed
   const callback = (mutationList, observer) => {
-      console.log(mutationList);
+    // console.log(mutationList);
     for (const mutation of mutationList) {
-      if (
-        mutation.type === "childList" &&
-        mutation.addedNodes.length &&
-        (mutation.addedNodes[0].id == `window-panel`||mutation.addedNodes[0].classList.contains('panel-group'))
-      ) {
-        setTimeout(() => {
-          const a = document.querySelector("#window-panel .tabsearch");
-          a && a.focus();
-        });
+      for (const i of mutation.addedNodes) {
+        if (i.id == "window-panel"||i.classList.contains("panel-group")) {
+          setTimeout(() => {
+            const a = document.querySelector("#window-panel .tabsearch");
+            a && a.focus();
+          });
+        }
       }
     }
   };
 
   // Create an observer instance linked to the callback function
   const observer = new MutationObserver(callback);
-
   // Start observing the target node for configured mutations
   observer.observe(targetNode, config);
-}, 2222);
+});
