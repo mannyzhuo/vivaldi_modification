1df60e8b4b1b5ee59ac2a06bbb971aea6a146dea
mannyzhuo <zgrbsbf@gmail.com>
2023-10-01 20:50:37 +0800

    Update auto_hide_address_bar_hover_show.js

--- auto_hide_address_bar_hover_show.js
+++ auto_hide_address_bar_hover_show.js
@@ -3,55 +3,23 @@
  * the path of browser.html in my PC is "C:\Users\xxxx\AppData\Local\Vivaldi\Application\6.3.3134.3\resources\vivaldi" 
  */
 
-setTimeout(() => {
+setTimeout(()=>{
+  let timer;
+  const f =(e)=>{
     const header = document.querySelector('#header');
-    const browser = document.querySelector('#browser');
-    const mainbar=document.querySelector('.mainbar');
-
-    let time=null;
-    let out=111;
-
-    function show(){
-        browser.classList.remove('address-top-off')
-        browser.classList.add('address-top')
-    }
-
-    function hide(){
-        browser.classList.add('address-top-off')
-        browser.classList.remove('address-top')
-    }
-
-    header.addEventListener('mouseenter',function(){
-        console.log('header enter');
-        if(time){
-            clearTimeout(time);
-            time=null;
-        }
-
-        const i = document.querySelector('#browser').classList.contains('address-top')
-        if(i){
-
-        }else{
-            show()
-        }
-     })
-     header.addEventListener('mouseleave',function(){
-        console.log('leave0');
-       time= setTimeout(function(){
-           hide();
-        },out);
-     })
-     mainbar.addEventListener('mouseenter',function(){
-        if(time){
-            clearTimeout(time);
-            time=null;
-        }
-     })
-     mainbar.addEventListener('mouseleave',function(){
-        console.log('mainbar leave');
-        time= setTimeout(function(){
-           hide();
-        });
-     })     
-}, 1111);
-
+    const mainbar = document.querySelector(".mainbar");
+    const browser = document.querySelector("#browser");
+    if(header && header.contains(e.target)|| mainbar&& mainbar.contains(e.target)){
+      browser.classList.remove("address-top-off");
+      browser.classList.add("address-top");
+    }else{
+      browser.classList.add("address-top-off");
+      browser.classList.remove("address-top");    }
+  };
+  document.addEventListener('pointermove',(e)=>{
+     if(timer){
+      clearTimeout(timer);
+     }
+     timer = setTimeout(f,0,e);
+  });
+});
