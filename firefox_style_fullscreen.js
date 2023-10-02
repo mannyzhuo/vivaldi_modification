60b2f4c7549b556ce8b81034d6b563a0384adc20
mannyzhuo <zgrbsbf@gmail.com>
2023-10-02 12:04:42 +0800

    Update firefox_style_fullscreen.js

--- firefox_style_fullscreen.js
+++ firefox_style_fullscreen.js
@@ -1,24 +1,33 @@
 let hr = setInterval(() => {
-  const c = document.querySelector('#webview-container');
+  const c = document.querySelector("#webview-container");
   const header = document.querySelector("#header");
-  const addressbar = document.querySelector('#main .mainbar');
-  if(c){
+  const browser = document.querySelector("#browser");
+
+  if (c) {
+    const style = document.createElement("style");
+    style.appendChild(document.createTextNode("[hidden] { display: none !important; }"));
+    document.head.appendChild(style);
+
     clearInterval(hr);
-    const div = document.createElement('div');
-    div.style.height='9px';
-    div.style.width='100vw';
-    div.style.position='fixed';
-    div.style.left='0';
-    div.style.top='0';
-    div.style.zIndex=1;
-    document.body.insertBefore(div,document.body.firstChild);
-    c.addEventListener('pointerenter',()=>{
-      header.style.display='none';
-      addressbar.style.display='none';
+    const div = document.createElement("div");
+    div.style.height = "1px";
+    div.style.width = "100vw";
+    div.style.position = "fixed";
+    div.style.left = "0";
+    div.style.top = "0";
+    div.style.zIndex = 1;
+    document.body.insertBefore(div, document.body.firstChild);
+    c.addEventListener("pointerenter", () => {
+      header.hidden=true;
+      browser.classList.remove("address-top");
+      browser.classList.add("address-top-off");
+      [...document.querySelectorAll('.tabbar-wrapper')].forEach(item=>item.hidden=true);
     });
-    div.addEventListener('pointerenter',(e)=>{
-      header.style.display='flex';
-      addressbar.style.display='block';
+    div.addEventListener("pointerenter", (e) => {
+      header.hidden=false;
+      [...document.querySelectorAll('.tabbar-wrapper')].forEach(item=>item.hidden=false);
+      browser.classList.remove("address-top-off");
+      browser.classList.add("address-top");
     });
   }
 }, 1111);
