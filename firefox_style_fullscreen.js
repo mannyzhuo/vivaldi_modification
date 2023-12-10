01c4d9c8a103ad4d07ed24cc26758de2b81aeb50
mannyzhuo <zgrbsbf@gmail.com>
2023-12-10 10:56:52 +0800

    Update firefox_style_fullscreen.js
     you must uncheck "show address bar" in the page of "vivaldi://settings/addressbar/"
    
    
    this script set "Alt+X" to toggle address bar and tab bar

--- firefox_style_fullscreen.js
+++ firefox_style_fullscreen.js
@@ -1,12 +1,34 @@
+// you must uncheck "show address bar" in the page of "vivaldi://settings/addressbar/"
+// this script set "Alt+X" to toggle address bar and tab bar
+
 let hr = setInterval(() => {
   const c = document.querySelector("#webview-container");
   const header = document.querySelector("#header");
   const browser = document.querySelector("#browser");
 
+  function show() {
+    header.hidden = false;
+    [...document.querySelectorAll(".tabbar-wrapper")].forEach(
+      (item) => (item.hidden = false)
+    );
+    browser.classList.remove("address-top-off");
+    browser.classList.add("address-top");
+  }
+
+  function toggle(){
+    browser.classList.contains("address-top")?hide():show()
+  }
+
   if (c) {
     const style = document.createElement("style");
-    style.appendChild(document.createTextNode("[hidden] { display: none !important; }"));
+    style.appendChild(
+      document.createTextNode("[hidden] { display: none !important; }")
+    );
     document.head.appendChild(style);
+
+    vivaldi.tabsPrivate.onKeyboardShortcut.addListener(
+      (id, combination) => combination === "Alt+X" && toggle()
+    );
 
     clearInterval(hr);
     const div = document.createElement("div");
@@ -18,25 +40,48 @@ let hr = setInterval(() => {
     div.style.zIndex = 1;
     document.body.insertBefore(div, document.body.firstChild);
 
-    function hide(){
+    function hide() {
+      header.hidden = true;
       browser.classList.remove("address-top");
       browser.classList.add("address-top-off");
+      [...document.querySelectorAll(".tabbar-wrapper")].forEach(
+        (item) => (item.hidden = true)
+      );
     }
     c.addEventListener("pointerenter", () => {
-      header.hidden=true;
       hide();
-      [...document.querySelectorAll('.tabbar-wrapper')].forEach(item=>item.hidden=true);
     });
-    div.addEventListener("pointerenter", (e) => {
-      header.hidden=false;
-      [...document.querySelectorAll('.tabbar-wrapper')].forEach(item=>item.hidden=false);
-      browser.classList.remove("address-top-off");
-      browser.classList.add("address-top");
+    c.addEventListener('click',(e)=>{
+      console.log('click');
     });
-    document.addEventListener("visibilitychange", () => {
-      if (!document.hidden) {
-         hide();
-      }
-    });
+    div.addEventListener("pointerenter",show);
   }
 }, 1111);
+
+setTimeout(() => {
+  // Select the node that will be observed for mutations
+  const targetNode = document;
+
+  // Options for the observer (which mutations to observe)
+  const config = { attributes: true, childList: true, subtree: true };
+
+  // Callback function to execute when mutations are observed
+  const callback = (mutationList, observer) => {
+    // console.log(mutationList);
+    for (const mutation of mutationList) {
+      for (const i of mutation.addedNodes) {
+        if (i.id == "window-panel" || i.classList?.contains("panel-group")) {
+          setTimeout(() => {
+            const a = document.querySelector("#window-panel .tabsearch");
+            a && a.focus();
+          });
+        }
+      }
+    }
+  };
+
+  // Create an observer instance linked to the callback function
+  const observer = new MutationObserver(callback);
+  // Start observing the target node for configured mutations
+  observer.observe(targetNode, config);
+});
